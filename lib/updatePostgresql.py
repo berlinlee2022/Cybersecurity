5684ce684930aec53f24a4719092629219883610
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-18 20:34:37 +0800

    updatePostgresql.py

--- lib/updatePostgresql.py
+++ lib/updatePostgresql.py
@@ -13,49 +13,71 @@ def updatePostgres():
         print(Fore.YELLOW + "\nUpdating PosgreSQL 15 port from port 5433 to port 5432...\n")
         
         updatePostgres15 = f'echo {sudo_password} | sudo sed -i \'s/port \= 5433/port \= 5432/g\' /etc/postgresql/15/main/postgresql.conf'
-        doUpdatePostgres15 = subprocess.Popen(updatePostgres15, shell=True)
-        doUpdatePostgres15.wait()
+        doUpdatePostgres15 = subprocess.Popen(updatePostgres15, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        doUpdatePostgres15_out, doUpdatePostgres15_err = doUpdatePostgres15.communicate()
 
         if doUpdatePostgres15.returncode == 0:
-            print(Fore.YELLOW + "Updating PosgreSQL 15 port from port 5433 to port 5432\nhas succeeded!\n")
-            print(doUpdatePostgres15.stdout)
+            print(f'\n')
+            print(Fore.WHITE + f'{doUpdatePostgres15_out}')
+            print(f'\n')
+            print(Fore.YELLOW + "Updating PosgreSQL 15 port from port 5433 to port 5432\nhas succeeded!")
+            print(f'\n')
+            print(f'\n')
         else:
+            print(f'\n')
+            print(Fore.WHITE + f'{doUpdatePostgres15_err}')
+            print(f'\n')
             print(Fore.RED + "Updating PosgreSQL 15 port from port 5433 to port 5432\nhas failed... :(")
-            print(doUpdatePostgres15.stderr)
+            print(f'\n')
+            print(f'\n')
 
         print(Fore.WHITE+ "Updating PosgreSQL 14 port from port 5432 to port 5433")
-        print(Style.RESET_ALL)
 
         # Changing port 5432 => 5433 for PostgreSQL14 to allow update
         #os.system('sudo sed -i \'s/port \= 5432/port \= 5433/g\' /etc/postgresql/15/main/postgresql.conf')
         updatePostgres14 = f'echo {sudo_password} | sudo sed -i \'s/port \= 5432/port \= 5433/g\' /etc/postgresql/14/main/postgresql.conf'
-        doUpdatePostgres14 = subprocess.Popen(updatePostgres14, shell=True)
-        doUpdatePostgres14.wait()
+        doUpdatePostgres14 = subprocess.Popen(updatePostgres14, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        doUpdatePostgres14_out, doUpdatePostgres14_err = doUpdatePostgres14.communicate()
 
         if doUpdatePostgres14 == 0:
-            print(Fore.YELLOW + "\nUpdating PosgreSQL 14 port from port 5432 to port 5433 has succeeded!\n")
-            print(Style.RESET_ALL)
-            print(doUpdatePostgres14.stdout)
+            print(f'\n')
+            print(Fore.WHITE + f'{doUpdatePostgres14_out}')
+            print(f'\n')
+            print(Fore.YELLOW + "\nUpdating PosgreSQL 14 port from port 5432 to port 5433 has succeeded!")
+            print(f'\n')
+            print(f'\n')
                 
         else:
-            print(Fore.RED + "\nFailed to update PostgreSQL 14 port no. :(\n")
-            print(doUpdatePostgres14.stderr)
-            print(Style.RESET_ALL)
+            print(f'\n')
+            print(Fore.WHITE + f'{doUpdatePostgres14_err}')
+            print(Fore.RED + "\nFailed to update PostgreSQL 14 port no. :(")
+            print(f'\n')
+            print(f'\n')
 
         # Restart PostgreSQL service
-        print(Fore.YELLOW + "\nRestarting PostgreSQL service...\n")
-        #os.system('sudo systemctl restart postgresql')
+        print(f'\n')
+        print(Fore.YELLOW + "Restarting PostgreSQL service...")
+        print(f'\n')
         restartPostgres = f'echo {sudo_password} | sudo systemctl restart postgresql'
-        doRestartPostgres = subprocess.Popen(restartPostgres, shell=True)
-        doRestartPostgres.wait()
+        doRestartPostgres = subprocess.Popen(restartPostgres, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        doRestartPostgres_out, doRestartPostgres_err = doRestartPostgres.communicate()
 
         if doRestartPostgres.returncode == 0:
-            print(Fore.YELLOW + "\nRestarting PostgreSQL service has succeeded!\n")
-            print(doRestartPostgres.stdout)
+            print(f'\n')
+            print(Fore.WHITE + f'{doRestartPostgres_out}')
+            print(f'\n')
+            print(Fore.YELLOW + "Restarting PostgreSQL service has succeeded!")
+            print(f'\n')
+            print(f'\n')
         else:
-            print(Fore.RED + "\nRestarting PostgreSQL service has failed...:(\n")
-            print(doRestartPostgres.stderr)
-            print(Style.RESET_ALL)
+            print(f'\n')
+            print(Fore.WHITE + f'{doRestartPostgres_err}')
+            print(f'\n')
+            print(f'\n')
+            print(Fore.RED + "Restarting PostgreSQL service has failed...:(")
+            print(f'\n')
+            print(f'\n')
     else:
+        print(f'\n')
         print(Fore.RED + f'Could NOT confirmUpdatePostgres is Y\nInput was {confirmUpdatePostgres}\nSkipping...\n')
     
