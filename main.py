d2721612c8e6e6fa0360ef721c15eec953f40d68
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-26 00:54:29 +0800

    main.py

--- main.py
+++ main.py
@@ -84,59 +84,62 @@ user, sudo_password = getCredentials()
 
 directory1 = f'/home/{user}'
 mkdir1 = f'echo {sudo_password} | sudo mkdir /home/{user}'
-do_mkdir1 = subprocess.run(
-    mkdir1, shell=True, 
-    stdout=subprocess.PIPE, 
-    stderr=subprocess.PIPE
+do_mkdir1 = subprocess.Popen(
+    mkdir1, 
+    shell=True, 
+    text=True
     )
+do_mkdir1.wait()
    
 if do_mkdir1.returncode == 0:
         
     print(f'\n')
-    print(Fore.WHITE + f'{do_mkdir1.stdout}')
+    print(Fore.WHITE + f'Succeeded in {do_mkdir1}')
     print(f'\n')
         
 else:
     print(f'\n')
-    print(Fore.WHITE + f'{do_mkdir1.stderr}')
+    print(Fore.WHITE + f'Failed {do_mkdir1}')
     print(f'\n')
 
 directory2 = f'/home/{user}/Desktop'
 mkdir2 = f'echo {sudo_password} | sudo mkdir /home/{user}'
 do_mkdir2 = subprocess.run(
-    mkdir2, shell=True, 
-    stdout=subprocess.PIPE, 
-    stderr=subprocess.PIPE
+    mkdir2, 
+    shell=True, 
+    text=True
     )
+do_mkdir2.wait()
    
 if do_mkdir2.returncode == 0:
         
     print(f'\n')
-    print(Fore.WHITE + f'{do_mkdir2.stdout}')
+    print(Fore.WHITE + f'Succeeded in {do_mkdir2}')
     print(f'\n')
         
 else:
     print(f'\n')
-    print(Fore.WHITE + f'{do_mkdir2.stderr}')
+    print(Fore.WHITE + f'Failed {do_mkdir2}')
     print(f'\n')
         
 directory3 = f'/home/{user}/Desktop/tools'
 mkdir3 = f'echo {sudo_password} | sudo mkdir /home/{user}'
 do_mkdir3 = subprocess.run(
-    mkdir3, shell=True, 
-    stdout=subprocess.PIPE, 
-    stderr=subprocess.PIPE
+    mkdir3, 
+    shell=True, 
+    text=True
     )
+do_mkdir3.wait()
    
 if do_mkdir3.returncode == 0:
         
     print(f'\n')
-    print(Fore.WHITE + f'{do_mkdir3.stdout}')
+    print(Fore.WHITE + f'Succeeded in {do_mkdir3}')
     print(f'\n')
         
 else:
     print(f'\n')
-    print(Fore.WHITE + f'{do_mkdir3.stderr}')
+    print(Fore.WHITE + f'Failed {do_mkdir3}')
     print(f'\n')
     
 
